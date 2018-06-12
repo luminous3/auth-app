329849390bedeaf76fccd31842a50a0b6ed90161
Alexandra Mirtcheva <alex@solanalabs.io>
2018-06-12 10:18:17 -0700

    firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: 'AIzaSyB29XsL5Obi-iDckCdQlPFihpjbtlRcUGw',
+  authDomain: 'auth-42754.firebaseapp.com',
+  databaseURL: 'https://auth-42754.firebaseio.com',
+  projectId: 'auth-42754',
+  storageBucket: 'auth-42754.appspot.com',
+  messagingSenderId: '651977891614',
+}
--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react'
 import firebase from 'firebase'
 import { View, Text } from 'react-native'
 import { Header } from './components/common'
+import { config } from '../firebaseConfig'
 
 class App extends Component {
   constructor(props) {
     super(props)
 
-    firebase.initializeApp({
-      apiKey: 'AIzaSyB29XsL5Obi-iDckCdQlPFihpjbtlRcUGw',
-      authDomain: 'auth-42754.firebaseapp.com',
-      databaseURL: 'https://auth-42754.firebaseio.com',
-      projectId: 'auth-42754',
-      storageBucket: 'auth-42754.appspot.com',
-      messagingSenderId: '651977891614',
-    })
+    firebase.initializeApp(config)
   }
 
   render() {
