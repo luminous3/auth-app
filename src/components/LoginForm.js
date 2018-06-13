adbe7a08b35540f5d6ef6f952dce2c3a9cb66984
Alexandra Mirtcheva <alex@solanalabs.io>
2018-06-13 05:49:12 -0700

    spinner and success/failure states

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
 import { Text } from 'react-native'
-import { Button, Card, CardSection, Input } from './common'
+import { Button, Card, CardSection, Input, Spinner } from './common'
 
 class LoginForm extends Component {
   state = {
     email: '',
     password: '',
     error: '',
+    loading: false,
   }
 
   onButtonPress() {
     const { email, password } = this.state
+    this.setState({ error: '', loading: true })
+
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
+      .then(() => this.onLoginSuccess.bind(this))
       .catch(() => {
-        firebase.auth().createUserWithEmailAndPassword(email, password)
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(() => {
+            this.setState(this.onLoginFail.bind(this))
+          })
       })
-      .catch(() => {
-        this.setState({ error: 'Authentication Failed.' })
-      })
+  }
+
+  onLoginFail() {
+    this.setState({ error: 'Authentication failed', loading: false })
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: '',
+    })
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />
+    }
+
+    return <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
   }
 
   render() {
@@ -47,9 +75,7 @@ class LoginForm extends Component {
 
         <Text style={styles.errorTextStyle}>{this.state.error}</Text>
 
-        <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
-        </CardSection>
+        <CardSection>{this.renderButton()}</CardSection>
       </Card>
     )
   }
