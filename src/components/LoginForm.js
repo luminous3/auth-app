8cf3174067c0108239de6aa1d09417fbc0c6f419
Alexandra Mirtcheva <alex@solanalabs.io>
2018-06-13 05:23:55 -0700

    auth with firebase and error handling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
+import { Text } from 'react-native'
 import { Button, Card, CardSection, Input } from './common'
 
 class LoginForm extends Component {
   state = {
     email: '',
     password: '',
+    error: '',
   }
 
   onButtonPress() {
     const { email, password } = this.state
-    firebase.auth().signInWithEmailAndPassword(email, password)
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+      })
+      .catch(() => {
+        this.setState({ error: 'Authentication Failed.' })
+      })
   }
 
   render() {
@@ -35,12 +45,22 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
         </CardSection>
       </Card>
     )
   }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
 }
 
 export default LoginForm
